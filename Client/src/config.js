587e0a299a6373c51b392902d40ff33284a73d8b
Kinji <linhqwertyuiop2gmail.com>
2024-04-08 15:32:41 +0700

    update post, get sample

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:8000/api";
+const JAVA_URL = "http://localhost:8080/api";
 
 const axiosInstance = axios.create({
   withCredentials: true,
   credentials: "include",
   baseURL: BASE_URL,
+});
+
+export const axiosJava = axios.create({
+  baseURL: JAVA_URL,
 });
 
 axiosInstance.defaults.withCredentials = true;
