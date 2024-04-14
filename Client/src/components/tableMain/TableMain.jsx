5b2991c5401779b38977148a91cfb0be97912b32
nguyenthanhtoan903 <codehub.personal@gmail.com>
2024-04-14 21:44:43 +0700

    update ui

--- Client/src/components/tableMain/TableMain.jsx
+++ Client/src/components/tableMain/TableMain.jsx
@@ -15,91 +15,123 @@ const TableMain = ({ data }) => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div className="table-home">
-      <Table
-        className="table-sample"
-        striped
-        bordered
-        hover
-        size="sm"
-        // style={{ maxWidth: '95%', marginLeft: 'auto', marginRight: 'auto' }}
-      >
-        <thead>
-          <tr style={{ textAlign: "center" }}>
-            <th>#</th>
-            <th>Thumbnail</th>
-            <th>Sample Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td style={{ textAlign: "center", alignContent: "center" }}>
-                  {index + 1}
-                </td>
-                <td style={{ textAlign: "center", alignContent: "center" }}>
-                  <Image
-                    src={item.thumbnail.path}
-                    rounded={true}
-                    style={{
-                      height: "80px",
-                      width: "80px",
-                      marginRight: "5px",
-                      border: "1px solid",
-                    }}
-                  />
-                </td>
-                <td style={{ textAlign: "left", alignContent: "center" }}>
-                  {item.name}
-                </td>
-                {/* <td style={{ textAlign: "center", alignContent: "center" }}>
-                  {item.size}
-                </td> */}
-                <td style={{ textAlign: "center", alignContent: "center" }}>
-                  {item.email}
-                </td>
-                <td
-                  className="action"
-                  style={{ textAlign: "center", alignContent: "center" }}
-                >
-                  <span
-                    className=""
-                    style={{ color: "red", marginLeft: "10px" }}
-                    onClick={() => {
-                      setSampleId(item.id);
-                      setModalShow(true);
-                      setPublicId(item.thumbnail.publicId);
-                    }}
-                  >
-                    <Trash />
-                  </span>
-                  <Link
-                    to={`/form?mode=edit&id=${item.id}`}
-                    className=""
-                    style={{ color: "Green", marginLeft: "10px" }}
-                  >
-                    <Edit />
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-
-      <DeleteModal
-        modalShow={modalShow}
-        setModalShow={setModalShow}
-        id={sampleId}
-        publicId={publicId}
-        setLoading={setLoading}
-      />
-      {loading && <Loading />}
+    <div>
+      {data?.map((item, index) => (
+        <div key={index} className="card" style={{ width: "100%" }}>
+          <div className="card-body" style={{ display: "flex" }}>
+            <div className="card-item">
+              <Image className="card-img" src={item.thumbnail.path} />
+            </div>
+            <div className="card-discription" style={{ flex: 1 }}>
+              <h2 className="card-title">{item.name}</h2>
+              <h4>Author: {item.email}</h4>
+              <h5>Size: {item.size}</h5>
+              <p className="card-text">{item.description}</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+              <span
+                className=""
+                style={{ color: "red", marginLeft: "10px" }}
+                onClick={() => {
+                  setSampleId(item.id);
+                  setModalShow(true);
+                  setPublicId(item.thumbnail.publicId);
+                }}
+              >
+                <Trash />
+              </span>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default TableMain;
+
+// <div className="table-home">
+//       <Table
+//         className="table-sample"
+//         striped
+//         bordered
+//         hover
+//         size="sm"
+//         // style={{ maxWidth: '95%', marginLeft: 'auto', marginRight: 'auto' }}
+//       >
+//         <thead>
+//           <tr style={{ textAlign: "center" }}>
+//             <th>#</th>
+//             <th>Thumbnail</th>
+//             <th>Sample Name</th>
+//             <th>Email</th>
+//             <th>Actions</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {data?.map((item, index) => {
+//             return (
+//               <tr key={index}>
+//                 <td style={{ textAlign: "center", alignContent: "center" }}>
+//                   {index + 1}
+//                 </td>
+//                 <td style={{ textAlign: "center", alignContent: "center" }}>
+//                   <Image
+//                     src={item.thumbnail.path}
+//                     rounded={true}
+//                     style={{
+//                       height: "80px",
+//                       width: "80px",
+//                       marginRight: "5px",
+//                       border: "1px solid",
+//                     }}
+//                   />
+//                 </td>
+//                 <td style={{ textAlign: "left", alignContent: "center" }}>
+//                   {item.name}
+//                 </td>
+//                 {/* <td style={{ textAlign: "center", alignContent: "center" }}>
+//                   {item.size}
+//                 </td> */}
+//                 <td style={{ textAlign: "center", alignContent: "center" }}>
+//                   {item.email}
+//                 </td>
+//                 <td
+//                   className="action"
+//                   style={{ textAlign: "center", alignContent: "center" }}
+//                 >
+//                   <span
+//                     className=""
+//                     style={{ color: "red", marginLeft: "10px" }}
+//                     onClick={() => {
+//                       setSampleId(item.id);
+//                       setModalShow(true);
+//                       setPublicId(item.thumbnail.publicId);
+//                     }}
+//                   >
+//                     <Trash />
+//                   </span>
+//                   <Link
+//                     to={`/form?mode=edit&id=${item.id}`}
+//                     className=""
+//                     style={{ color: "Green", marginLeft: "10px" }}
+//                   >
+//                     <Edit />
+//                   </Link>
+//                 </td>
+//               </tr>
+//             );
+//           })}
+//         </tbody>
+//       </Table>
+
+//       <DeleteModal
+//         modalShow={modalShow}
+//         setModalShow={setModalShow}
+//         id={sampleId}
+//         publicId={publicId}
+//         setLoading={setLoading}
+//       />
+//       {loading && <Loading />}
+//     </div>
